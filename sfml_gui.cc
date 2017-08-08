ce28fe8a89170513ec34590791a12ea73fb9d7fb
Antti Alhonen <antti.alhonen@iki.fi>
2017-08-08 13:34:31 +0300

    gui files

---
+++ sfml_gui.cc
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <SFML/Graphics.hpp>
+
+#include "sfml_gui.h"
+
+using namespace std;
+
+sfml_gui::sfml_gui(sf::RenderWindow& win, sf::Font& fo) : window(win), font(fo)
+{
+	num_buttons = 0;
+}
+
+void sfml_gui::change_text(int num, const char* text)
+{
+	delete buttons[num]->text;
+	buttons[num]->text = new char[strlen(text)+1];
+	strcpy(buttons[num]->text, text);
+}
+
+int sfml_gui::add_button(int x, int y, int xs, int ys, const char* text, sf::Color color, int font_size,
+			 int symbol, sf::Color color_pressed, bool pressed)
+{
+	buttons[num_buttons] = new button;
+	buttons[num_buttons]->x = x;
+	buttons[num_buttons]->y = y;
+	buttons[num_buttons]->xs = xs;
+	buttons[num_buttons]->ys = ys;
+	buttons[num_buttons]->text = new char[strlen(text)+1];
+	strcpy(buttons[num_buttons]->text, text);
+	buttons[num_buttons]->visible = true;
+	buttons[num_buttons]->font_size = font_size;
+	buttons[num_buttons]->color = color;
+	buttons[num_buttons]->symbol = symbol;
+	buttons[num_buttons]->pressed = pressed;
+	buttons[num_buttons]->color_pressed = color_pressed;
+	buttons[num_buttons]->text_color = sf::Color(0,0,0);
+
+	num_buttons++;
+
+	return num_buttons-1;
+}
+
+void sfml_gui::hide_button(int num)
+{
+	buttons[num]->visible = false;
+}
+
+void sfml_gui::show_button(int num)
+{
+	buttons[num]->visible = true;
+}
+
+void sfml_gui::draw_button(int num)
+{
+	if(buttons[num]->visible == false)
+	return;
+
+	const sf::Color line_color = sf::Color(0,0,0);
+	const int shadow_offset = 2;
+	const sf::Color shadow_color = sf::Color(100,100,100);
+
+	// Draw shadow first
+
+	sf::RectangleShape rect(sf::Vector2f(buttons[num]->xs, buttons[num]->ys));
+	rect.setPosition(buttons[num]->x+shadow_offset, buttons[num]->y+shadow_offset);
+	rect.setFillColor(shadow_color);
+	rect.setOutlineThickness(2.0);
+	rect.setOutlineColor(shadow_color);
+
+	window.draw(rect);
+
+	int button_offset = (buttons[num]->pressed)?shadow_offset:0;
+	rect.setPosition(buttons[num]->x+button_offset, buttons[num]->y+button_offset);
+	rect.setFillColor((buttons[num]->pressed)?(buttons[num]->color_pressed):(buttons[num]->color));
+	rect.setOutlineColor(line_color);
+
+	window.draw(rect);
+
+	int text_offset = 0;
+	const int sym_xoffs = 5;
+	const int sym_yoffs = 6;
+	if(buttons[num]->symbol > -1)
+	{
+		for(int poly = 0; poly < SYMBOLS[buttons[num]->symbol].n_polys; poly++)
+		{
+			sf::ConvexShape shape;
+			if(SYMBOLS[buttons[num]->symbol].polys[poly].line) shape.setOutlineThickness(SYMBOLS[buttons[num]->symbol].polys[poly].line_width);
+			shape.setFillColor(SYMBOLS[buttons[num]->symbol].polys[poly].fill_color);
+			shape.setOutlineColor(SYMBOLS[buttons[num]->symbol].polys[poly].line_color);
+			shape.setPointCount(SYMBOLS[buttons[num]->symbol].polys[poly].n_points);
+
+			for(int i = 0; i < SYMBOLS[buttons[num]->symbol].polys[poly].n_points; i++)
+			{
+				shape.setPoint(i, sf::Vector2f(SYMBOLS[buttons[num]->symbol].polys[poly].points[i*2],
+					SYMBOLS[buttons[num]->symbol].polys[poly].points[i*2+1]));
+			}
+
+			shape.setPosition(buttons[num]->x+sym_xoffs+button_offset, buttons[num]->y+sym_yoffs+button_offset);
+			window.draw(shape);
+			text_offset = SYMBOLS[buttons[num]->symbol].text_offset;
+		}
+	}
+
+	sf::Text t;
+	t.setFont(font);
+	t.setCharacterSize(buttons[num]->font_size);
+	t.setString(buttons[num]->text);
+	t.setColor(buttons[num]->text_color);
+	t.setPosition(buttons[num]->x+5+text_offset+button_offset, buttons[num]->y+3+button_offset);
+	window.draw(t);
+}
+
+void sfml_gui::draw_all_buttons()
+{
+	for(int i = 0; i < num_buttons; i++)
+	{
+		draw_button(i);
+	}
+}
+
+int sfml_gui::check_button_status()
+{
+	unsigned int x, y;
+	bool mouse;
+
+	x = sf::Mouse::getPosition(window).x;
+	y = sf::Mouse::getPosition(window).y;
+	mouse = sf::Mouse::isButtonPressed(sf::Mouse::Left);
+
+	if(mouse) // && x > 0 && y > 0 && x < window_xsize && y < window_ysize)
+	{
+		for(int i = 0; i < num_buttons; i++)
+		{
+			if(x > buttons[i]->x && x < buttons[i]->x+buttons[i]->xs &&
+			   y > buttons[i]->y && y < buttons[i]->y+buttons[i]->ys &&
+			buttons[i]->visible)
+			return i;
+		}
+	}
+
+	return -1;
+
+}
+
+/*
+void sfml_gui::query_mouse(int* x, int* y, bool* left, bool* mid, bool* right)
+{
+	*x = input.GetMouseX();
+	*y = input.GetMouseY();
+	if(left != 0)
+	*left = input.IsMouseButtonDown(sf::Mouse::Left);
+	if(mid != 0)
+	*mid = input.IsMouseButtonDown(sf::Mouse::Middle);
+	if(right != 0)
+	*right = input.IsMouseButtonDown(sf::Mouse::Right);
+}
+*/
+
+void sfml_gui::destroy_buttons()
+{
+	for(int i = 0; i < num_buttons; i++)
+	{
+		delete buttons[i]->text;
+		delete buttons[i];
+		buttons[i]->text = 0;
+		buttons[i] = 0;
+	}
+
+	num_buttons = 0;
+}
+
---
+++ sfml_gui.h
@@ -0,0 +1,113 @@
+#ifndef UI_HPP
+#define UI_HPP
+
+#include <SFML/Graphics.hpp>
+
+#define MAX_BUTTONS	 64
+
+struct button
+{
+	int x;
+	int y;
+	int xs;
+	int ys;
+	bool visible;
+	//	int state;
+	bool pressed;
+	char* text;
+	int symbol;
+	int font_size;
+	sf::Color color;
+	sf::Color color_pressed;
+	sf::Color text_color;
+};
+
+#define SYMB_MAX_POLYS   5
+#define POLY_MAX_POINTS 20*2
+
+struct poly
+{
+	int n_points;
+	sf::Color fill_color;
+	bool line;
+	int line_width;
+	sf::Color line_color;
+	int points[POLY_MAX_POINTS];
+};
+
+struct symbol
+{
+	int n_polys;
+	poly polys[SYMB_MAX_POLYS];
+	int text_offset;
+};
+
+
+const symbol SYMBOLS[] = 
+{     // nPoint fill? fill color            line?  W  line color            points (x,y,x,y,...)          text offset
+	1, {3, sf::Color(  0,180,  0), true, 1, sf::Color(  0, 80,  0), {0,0, 0,10, 10,5}},           15,	    // 0 = PLAY
+	1, {4, sf::Color(180,  0,  0), true, 1, sf::Color( 80,  0,  0), {0,0, 0,10, 10,10, 10,0}},    15,	    // 1 = STOP
+	1, {3, sf::Color(  0,200,200), true, 1, sf::Color(  0, 90, 90), {0,2, 0,8,   8,5}},           15,	    // 2 = PLAYSLOW
+	1, {3, sf::Color(110,255,110), true, 1, sf::Color( 30, 60, 30), {0,0, 0,10, 10,5}},           15,	    // 3 = ROUTE
+	1, {3, sf::Color(235,235,110), true, 1, sf::Color( 60, 60, 30), {0,0, 0,10, 10,5}},           15,	    // 4 = MANUAL
+	1, {3, sf::Color(255,110,110), true, 1, sf::Color( 60, 30, 30), {0,0, 0,10, 10,5}},           15,	    // 5 = FORCE
+	1, {3, sf::Color(110,200,200), true, 1, sf::Color( 30, 60, 60), {0,0, 0,10, 10,5}},           15	    // 6 = POSE
+};
+
+#define SYM_PLAY 0
+#define SYM_STOP 1
+#define SYM_PLAYSLOW 2
+#define SYM_ROUTE  3
+#define SYM_MANUAL 4
+#define SYM_FORCE  5
+#define SYM_POSE   6
+
+const sf::Color DEF_BUT_COL = sf::Color(200,200,200);
+const sf::Color DEF_BUT_COL_PRESSED = sf::Color(128,128,128);
+const int DEF_BUT_FONT_SIZE = 14;
+
+// Multiple Windows or RenderWindows each need their own sfml_gui objects.
+
+struct sfml_gui
+{
+	sfml_gui(sf::RenderWindow& win, sf::Font& fo);
+
+	sf::RenderWindow& window;  
+
+	sf::Font font;
+
+//	int window_xsize; 
+//	int window_ysize; 
+
+	int num_buttons;
+
+	button* buttons[MAX_BUTTONS];
+	//	checkbox checkboxes[MAX_CHECKBOXES];
+
+
+	// Returns the tag value of the new button.
+	int add_button(int x, int y, int xs, int ys, const char* text, sf::Color color = DEF_BUT_COL, int font_size = DEF_BUT_FONT_SIZE,  
+		   int symbol = -1, sf::Color color_pressed = DEF_BUT_COL_PRESSED, bool pressed = false);
+
+	void destroy_buttons();
+
+	void show_button(int num);
+	void hide_button(int num);
+
+	// Draws the button on screen
+	void draw_button(int num);
+
+	// Draws all the buttons on screen. Calls draw_button.
+	void draw_all_buttons();
+
+	// Returns the number of button pressed, or -1 if no button is pressed
+	int check_button_status();
+
+//	void query_mouse(int* x, int* y, bool* left, bool* mid = 0, bool* right = 0);
+
+	void change_text(int num, const char* text);
+
+};
+
+#endif
+
